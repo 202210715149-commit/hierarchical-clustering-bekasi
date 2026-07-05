8c93db9ce7b78780c6c2a976cd293ac97927b84c
202210715149-commit <202210715149@mhs.ubharajaya.ac.id>
2026-07-05 21:31:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -378,14 +378,6 @@ Hasil pengelompokan membagi 12 kecamatan menjadi 3 kelompok berdasarkan kemiripa
 }
 
     df_cluster["Interpretasi"] = df_cluster["Cluster"].map(interpretasi)
-    # Menambahkan interpretasi cluster
-interpretasi = {
-    1: "Tinggi",
-    2: "Rendah",
-    3: "Sedang"
-}
-
-df_cluster["Interpretasi"] = df_cluster["Cluster"].map(interpretasi)
 
 # ============================
 # Tabel
